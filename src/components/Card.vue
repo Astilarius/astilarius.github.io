ee724c00dce57df1c6d20f3a420ed46d9196f8b0
Ruslan Pronko <ruslan.pronko@silkdata.ai>
2025-03-25 15:37:22 +0300

    Add cards block

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    title: String,
+    image: {},
+})
+</script>
+
+<template>
+    <div class="relative">
+        <img :src="image" :alt="title" />
+        <span class="absolute bottom-3 left-2 max-w-[85%] text-white text-xs">{{ title }}</span>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/CardsBlock.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Card from "./Card.vue"
+import art_creation from './../assets/cards/art_creation.png'
+import exclusive_styles from '../assets/cards/exclusive_styles.png'
+import magic_avatars from './../assets/cards/magic_avatars.png'
+</script>
+
+<template>
+    <div class="flex flex-row gap-1">
+        <Card :image="art_creation" title="Unlimited Art Creation"/>
+        <Card :image="exclusive_styles" title="Exclusive Styles"/>
+        <Card :image="magic_avatars" title="Magic Avatars With 20% Off"/>
+    </div>
+</template>
+
+<style scoped>
+</style>
