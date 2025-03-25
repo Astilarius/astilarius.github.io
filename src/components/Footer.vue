fe8ebc3d3077abc62cdfabfcc2f9d7bc1ce4edf9
Ruslan Pronko <ruslan.pronko@silkdata.ai>
2025-03-25 15:37:42 +0300

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-row gap-14 text-xs font-extralight justify-center underline text-white/30">
+        <a>Terms of Use</a>
+        <a>Privacy Policy</a>
+        <a>Restore</a>
+    </div>
+</template>
+
+<style scoped>
+</style>
