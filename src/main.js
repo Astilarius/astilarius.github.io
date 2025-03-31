d4dd76072eb7e667a4cc36ce5b5dec278ead41a8
Ruslan Pronko <ruslan.pronko@silkdata.ai>
2025-03-31 22:02:15 +0300

    add i18n

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
+import de from './locales/de.json'
+import en from './locales/en.json'
+import es from './locales/es.json'
+import fr from './locales/fr.json'
+import ja from './locales/ja.json'
+import pt from './locales/pt.json'
 
-createApp(App).mount('#app')
+console.log(en['Terms of Use'])
+
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'de',
+    messages: {
+        de: de,
+        en: en,
+        es: es,
+        fr: fr,
+        ja: ja,
+        pt: pt
+    }
+})
+
+createApp(App).use(i18n).mount('#app')
