27114495ca00b108d0804c5b0dd4a6b35eba2fb2
Ruslan Pronko <ruslan.pronko@silkdata.ai>
2025-04-01 16:07:42 +0300

    move vlack gradient to its own block

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -42,6 +42,7 @@ const images = [
     <div class="background">
         <div class="background__blur"/>
         <div class="background__gradient"/>
+        <div class="background__dark-gradient"/>
         <div class="background__wrapper">
             <div class="background__table">
                 <div v-for="column in images" class="background__column">
@@ -78,20 +79,24 @@ const images = [
         background: linear-gradient(136.59deg, #DA28FF 3.34%, #7FCCFF 92.15%);
     }
 
+    .background__dark-gradient {
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        background: linear-gradient(196.56deg, rgba(0, 0, 0, 9.5e-05) 10.43%, rgba(21, 5, 52, 0.641925) 30.66%, rgba(0, 0, 0, 0.855327) 37.66%, rgba(9, 1, 25, 0.95) 45.57%, rgba(0, 0, 0, 0.95) 77.38%, rgba(5, 1, 14, 0.95) 94.32%);
+    }
+
     .background__wrapper {
         position: absolute;
         width: 100vw;
-        height: 113vh;
-        max-height: 844px;
-        top: -13vh;
+        height: 100vh;
         overflow: hidden;
-        background: linear-gradient(196.56deg, rgba(0, 0, 0, 9.5e-05) 10.43%, rgba(21, 5, 52, 0.641925) 30.66%, rgba(0, 0, 0, 0.855327) 37.66%, rgba(9, 1, 25, 0.95) 45.57%, rgba(0, 0, 0, 0.95) 77.38%, rgba(5, 1, 14, 0.95) 94.32%);
 
         .background__table {
             z-index: -10;
             position: absolute;
-            bottom: calc(-220.5px + 55.15%);
-            left: -2.75rem;
+            bottom: calc(-220.5px + 63%);
+            left: -2.5rem;
             display: flex;
             flex-direction: row;
             width: 100%;
