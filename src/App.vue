a1f51997ec4b81d173785a8b191e2b6e5862add7
Ruslan Pronko <ruslan.pronko@silkdata.ai>
2025-04-01 16:09:09 +0300

    Add App.vue styles

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,77 @@ import Background from "./components/Background.vue";
 import Footer from "./components/Footer.vue";
 import ButtonsBlock from "./components/ButtonsBlock.vue";
 import CardsBlock from "./components/CardsBlock.vue";
+import {useI18n} from "vue-i18n";
+import {onMounted} from "vue";
 
+const { locale } = useI18n();
+
+function getQueryParam(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+const routeLang = getQueryParam('lang');
+
+onMounted(() => {
+    if (routeLang) {
+        locale.value = routeLang;
+    }
+});
 </script>
 
 <template>
     <Background/>
-    <div class="px-6 flex flex-col gap-2">
-        <h1 class="text-center text-5xl font-extrabold text-white pt-40">Get Unlimited Access</h1>
+    <div class="content" :class="routeLang">
         <a href="#" class="cross-button">
             <img src="./assets/cross.svg"/>
         </a>
+        <h1 class="page-title" v-html="$t('Get Unlimited <br>Access')" />
         <CardsBlock/>
         <ButtonsBlock/>
         <Footer/>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@import "style.scss";
 
+
+.content {
+    padding: 0 1.5rem 0 1.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    justify-content: flex-end;
+    height: 100%;
+
+    .cross-button {
+        position: absolute;
+        width: 0.75rem;
+        height: 0.75rem;
+        padding: 0.375rem;
+        top: 37px;
+        left: 16px;
+        @media only screen and (min-height: 844px) {
+            top: 64px;
+        }
+    }
+
+    .page-title {
+        margin: 0 0 0.5rem 0;
+        text-align: center;
+        font-size: 3rem;
+        line-height: 1;
+        font-weight: 800;
+        color: $white;
+        @media only screen and (min-height: 844px) {
+            margin: 0 0 1.5rem 0;
+        }
+    }
+}
+.ja .page-title {
+    @media only screen and (max-height: 667px) {
+        font-size: 2.75rem;
+    }
+}
 </style>
